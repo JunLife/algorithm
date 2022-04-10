885e36e5a234b4830e8268ccf73ade1779b18eda
JunLife <alswns9288@gmail.com>
2022-04-10 22:57:16 +0900

    [ADD] 10870 해결

--- baekjoon/10870.py
+++ baekjoon/10870.py
@@ -3,8 +3,10 @@ import sys
 n = int(sys.stdin.readline())
 
 def recursion(n):
-    if n == 2 or n == 1 or n == 0:
+    if n == 2 or n == 1:
         return 1
+    if n == 0:
+        return 0
     
     return recursion(n - 1) + recursion(n - 2)
 
