c948d42458ba7b995c1a924e933f465ba304bbb9
JunLife <alswns9288@gmail.com>
2022-04-19 09:16:02 +0900

    [ADD] 18258 해결

---
+++ baekjoon/18258.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+if __name__ == '__main__':
+    n = int(sys.stdin.readline())
+    q = deque()
+    
+    for _ in range(n):
+        command = sys.stdin.readline().strip().split()
+
+        if len(command) > 1:
+            q.append(int(command[1]))
+        else:
+            if command[0] == 'pop':
+                if q:
+                    print(q.popleft())
+                else:
+                    print(-1)
+            elif command[0] == 'size':
+                print(len(q))
+            elif command[0] == 'empty':
+                if q:
+                    print(0)
+                else:
+                    print(1)
+            elif command[0] == 'front':
+                if q:
+                    print(q[0])
+                else:
+                    print(-1)
+            elif command[0] == 'back':
+                if q:
+                    print(q[-1])
+                else:
+                    print(-1)
