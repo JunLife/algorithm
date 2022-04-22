1c7b221e2d69dccacc579be1408078694622cfeb
JunLife <alswns9288@gmail.com>
2022-04-22 09:19:18 +0900

    [ADD] 9184 해결

--- baekjoon/9184.py
+++ baekjoon/9184.py
@@ -11,10 +11,10 @@ def w(a, b, c):
         return memo[a][b][c]
     
     if a < b and b < c:
-        memo[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        memo[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
         return memo[a][b][c]
     
-    memo[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    memo[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
     return memo[a][b][c]
 
 if __name__ == '__main__':
