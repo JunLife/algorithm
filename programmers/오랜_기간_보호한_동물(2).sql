44160dc2a7312caa8c24bc626593384217c18c83
JunLife <alswns9288@gmail.com>
2022-05-01 14:02:05 +0900

    [ADD] 오랜 기간 보호한 동물(2) 해결

---
+++ programmers/오랜_기간_보호한_동물(2).sql
@@ -0,0 +1,6 @@
+select ins.animal_id, ins.name
+from animal_ins as ins
+join animal_outs as outs
+on ins.animal_id = outs.animal_id
+order by timestampdiff(second, ins.datetime, outs.datetime) desc
+limit 2
